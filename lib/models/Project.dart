cecaaf5ce941b5ad5e65c9b39c886a21040854ce
Jimmy Nyamawi <nyamawijimmy@gmail.com>
2023-04-15 21:51:18 +0300

    first commit

---
+++ lib/models/Project.dart
@@ -0,0 +1,25 @@
+class Project {
+  final String? title, description;
+
+  Project({this.title, this.description});
+}
+
+List<Project> demo_projects = [
+  Project(
+    title: "Payment/My Bills Flutter App ",
+    description:
+    "Payment or Bills App helps you track expenses, transactions, monthly budgets, account balances, subscriptions, expenses and taxes. We calculate your net worth and spending trends and help you with your budget plan. We'll help manage subscriptions and notify you when prices go up. " ),
+  Project(
+    title: "Delivery /Ecommerce Flutter App",
+    description:
+    "An ecommerce or Delivery app allows users to shop online, browse product catalogs, create wish lists, add items to a cart, and complete purchases. It also provides payment processing, shipping, and order management capabilities " ),
+  Project(
+    title: "Tic Tac Toe Game Flutter App",
+    description:
+    "Tic Tac Toe Free supports one player and two player gameplay, so you can play against another human or against your iPhone. The AI for one player mode includes three difficulty levels, so you can play against a computer player that matches your skill level. " ),
+  Project(
+      title: "Flight Booking Flutter App",
+      description:
+"Users can simply sign up on the flight booking app with their email id, search through multiple flights and airlines available for their destination, modify their search according to their preferences, choose the right flight for their travel, and simply book their flight by paying through an online secure payment"
+  ),
+];
---
+++ lib/screens/main/components/side_menu.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+import '../../../Widgets/download.dart';
+import '../../../constants.dart';
+import 'area_info_text.dart';
+import 'coding.dart';
+import 'knowledges.dart';
+import 'my_info.dart';
+import 'skills.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      width: 350,
+      child: SafeArea(
+        child: Column(
+          children: [
+            MyInfo(),
+            Expanded(
+              child: SingleChildScrollView(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Column(
+                  children: [
+                    AreaInfoText(
+                      title: "Residence",
+                      text: "Kenya",
+                    ),
+                    AreaInfoText(
+                      title: "City",
+                      text: "Malindi",
+                    ),
+                    AreaInfoText(
+                      title: "Age",
+                      text: "22",
+                    ),
+                    Skills(),
+                    SizedBox(height: defaultPadding),
+                    Coding(),
+                    Knowledges(),
+                    Divider(),
+                    SizedBox(height: defaultPadding / 2),
+                    Container(
+                      margin: EdgeInsets.only(top: defaultPadding),
+                      color: Color(0xFF24242E),
+                      child: Row(
+                        children: [
+                          Spacer(),
+                          IconButton(
+                            onPressed: () {
+                              launch("https://github.com/Nyamawi-Jimmy");
+                            },
+                            icon: SvgPicture.asset("assets/icons/github.svg"),
+                          ),
+                          IconButton(
+                            onPressed: () async {
+                              final message = 'Hello!';
+                              final phone = '+254718845069';
+                              final url = 'whatsapp://send?phone=$phone&text=$message';
+                              if (await canLaunch(url)) {
+                                await launch(url);
+                              } else {
+                                throw 'Could not launch $url';
+                              }
+                            },
+                            icon: SvgPicture.asset("assets/icons/whatsapp.svg",color: Colors.grey,),
+                          ),
+                          Spacer(),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
